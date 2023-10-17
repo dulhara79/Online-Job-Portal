aeb82cad09b9adaba20a555edb723e581e50a2dd
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2023-10-17 21:51:45 +0530

    updateDeletefile

--- php/deleteCompany.php
+++ php/deleteCompany.php
@@ -1,16 +1,16 @@
 <?php
-    include "conn.php";
+    include "config.php";
 
     if(isset($_GET['deleteid'])) {
        $id=$_GET['deleteid'];
 
        $sql="DELETE FROM reg_user WHERE user_id = '$id'";
-       $result = mysqli_query($conn, $sql);
+       $result = mysqli_query($con, $sql);
        if($result == TRUE) {
           echo 'Deleted successfully!';
        }
        else {
-        die(mysqli_error($conn));
+        die(mysqli_error($con));
        }
     }
 ?>
